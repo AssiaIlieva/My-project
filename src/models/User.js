f50f24b823fbf96e410ee936041b24f87f102bbf
AssiaIlieva <ac17@abv.bg>
2024-06-23 15:26:51 +0300

    Update the User model

--- src/models/User.js
+++ src/models/User.js
@@ -4,15 +4,19 @@ const bcrypt = require('bcrypt');
 const userSchema = new Schema({
     username: {
         type: String,
+        minLength: 2,
+        maxLength: 20,
         required: [true, 'Username is required'],
     },
     email: {
         type: String,
+        minLength: 10,
         required: [true, 'Email is required'],
         unique: true,
     },
     password: {
         type: String,
+        minLength: 4,
         required: [true, 'Password is required'],
     }
 });
