2d5cfed3753491c686ba00141cafeb9b83170783
AssiaIlieva <ac17@abv.bg>
2024-06-23 15:27:32 +0300

    Delete the check auth function in Home Controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
-const {isAuth} = require('../middlewares/authMiddlware');
+
 
 router.get('/', (req, res) => {
     res.render('home')
 });
 
-// TODO: Delete this
-router.get('/authorize-test', isAuth, (req, res) => {
-    console.log(req.user);
-    res.send('Your are authorized');
-})
+
 
 module.exports = router
