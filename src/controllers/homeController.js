02170a18b95178025982224fc5b639c0b236ca86
AssiaIlieva <ac17@abv.bg>
2024-06-24 15:31:23 +0300

    Update the home view and home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
+const recipeService = require('../services/recipeService');
 
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/', async (req, res) => {
+    const latestRecipes = await recipeService.getLatest().lean();
+    res.render('home', {latestRecipes})
 });
 
-
+router.get('/search', async (req, res) => {
+    const {title} = req.query;
+    
+    let recipes = []
+    if(title){
+        recipes = await recipeService.search(title).lean();
+    }else{
+        recipes = await recipeService.getAll().lean();
+    }
+    res.render('search',{recipes, title})
+})
 
 module.exports = router
